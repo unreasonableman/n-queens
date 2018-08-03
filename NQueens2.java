01703038f9afe90dc2f9c938e8463deee906f007
Unreasonable Man <kay.neuenhofen@gmail.com>
2018-08-03 10:04:33 -0700

    - find all solutions for a given number of queens - make display of boards optional - optionally limit results to fundamental solutions (eliminate rotations   or mirrors of already found solutions) - make checking for groups of three or more colinear queens optional   (classic N queens compatibility mode

--- NQueens2.java
+++ NQueens2.java
@@ -1,11 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class NQueens2 {
+	private static final String N_ARG = "-n";
+	private static final String RESULT_ARG = "-display-results";
+	private static final String COLINEAR_ARG = "-allow-colinear-boards";
+	private static final String SOLUTION_ARG = "-all-solutions";
+
+	private static boolean displayResults;
+	private static boolean colinearBoards;
+	private static boolean fundamentalSolutions = true;
+
 	private int[] xyList;
+	private List<int[]> solutionList;
+	private int[] outList;
 
 	// place queens on board avoiding both direct attacks and
 	// instances of 3 or more colinear queens. One queen is placed per
 	// row. Backtracking occurs when a queen can't be placed in a row.
 	public NQueens2(int n) {
 		long then = System.currentTimeMillis();
+
+		solutionList = new ArrayList<int[]>();
+		outList = new int[n];
 
 		AttackMap attackMap = new AttackMap(n);
 		int[] queenIndexList = new int[n];
@@ -19,7 +36,14 @@ public class NQueens2 {
 		}
 
 		for (;;) {
-			if (currentQueenIndex >= n) break;
+			if (currentQueenIndex >= n) {
+				//dumpBoard(queenIndexList);
+				checkSolutionForUniqueness(queenIndexList);
+
+				currentQueenIndex--;
+				x = queenIndexList[currentQueenIndex];
+				attackMap.removeLastPiece(x, currentQueenIndex);
+			}
 
 			x = ++queenIndexList[currentQueenIndex];
 
@@ -32,18 +56,128 @@ public class NQueens2 {
 				x = queenIndexList[currentQueenIndex];
 				attackMap.removeLastPiece(x, currentQueenIndex);
 			} else {
-				if (attackMap.isAvailable(x, currentQueenIndex) && !hasColinearPoints(queenIndexList, currentQueenIndex + 1, n)) {
+				if (attackMap.isAvailable(x, currentQueenIndex) && (colinearBoards || !hasColinearPoints(queenIndexList, currentQueenIndex + 1, n))) {
 					attackMap.placePiece(x, currentQueenIndex);
 					currentQueenIndex++;
 				}
 			}
 		}
 
-		if (currentQueenIndex == 0) {
-			System.out.println("no solution found");
+		System.out.println(solutionList.size() + " solutions for " + n + " queens found in " + (System.currentTimeMillis() - then) + " ms.\n");
+
+		if (displayResults) {
+			for (int[] solution : solutionList) {
+				dumpBoard(solution);
+			}
+		}
+	}
+
+	private boolean indexListsEqual(int[] list1, int[] list2) {
+		if (list1.length != list2.length) return false;
+
+		for (int i=0; i<list1.length; i++) {
+			if (list1[i] != list2[i]) return false;
+		}
+
+		return true;
+	}
+
+	private void dumpIndexList(String prefix, int[] list) {
+		System.out.print(prefix);
+
+		for (int n : list) {
+			System.out.print(n + " ");
+		}
+
+		System.out.println();
+	}
+
+	private boolean doesRotationSolve(int[] list, int[] solution) {
+		if (indexListsEqual(solution, list)) {
+			return true;
+		}
+
+		flipIndexListHorizontally(list, outList);
+
+		if (indexListsEqual(solution, outList)) {
+			return true;
+		}
+
+		flipIndexListVertically(list, outList);
+
+		if (indexListsEqual(solution, outList)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	// x' = n - 1 - y
+	// y' = x
+	private void rotateIndexListClockWise(int[] inList, int[] outList) {
+		//System.out.println("NQueens2.rotateIndexListClockWise()");
+		//dumpIndexList("- inList: ", inList);
+
+		for (int i=0; i<inList.length; i++) {
+			outList[inList[i]] = inList.length - 1 - i;
+		}
+
+		//dumpIndexList("- outList: ", outList);
+	}
+
+	private void flipIndexListVertically(int[] inList, int[] outList) {
+		for (int i=0; i<inList.length; i++) {
+			outList[i] = inList[inList.length - 1 - i];
+		}
+	}
+
+	private void flipIndexListHorizontally(int[] inList, int[] outList) {
+		for (int i=0; i<inList.length; i++) {
+			outList[i] = inList.length - 1 - inList[i];
+		}
+	}
+
+	private void checkSolutionForUniqueness(int[] source) {
+		//System.out.println("NQueens2.checkSolutionForUniqueness()");
+		//System.out.println("- solutionList.size(): " + solutionList.size());
+
+		int[] list = new int[source.length];
+		System.arraycopy(source, 0, list, 0, source.length);
+
+		if (fundamentalSolutions) {
+			int[] tmp;
+
+
+			//dumpIndexList("- list: ", list);
+
+			for (int[] solution : solutionList) {
+				//dumpIndexList("-- solution: ", solution);
+
+				for (int i=0; i<4; i++) {
+					if (doesRotationSolve(solution, list)) {
+						return;
+					}
+
+					if (i == 3) break;
+
+					rotateIndexListClockWise(list, outList);
+
+					tmp = list;
+					list = outList;
+					outList = tmp;
+				}
+			}
+
+			//System.out.println("* rotations complete");
+			//dumpIndexList("- list: ", list);
+
+			solutionList.add(list);
 		} else {
-			System.out.println("solution found in " + (System.currentTimeMillis() - then) + " ms.");
-			dumpBoard(queenIndexList);
+			for (int[] solution : solutionList) {
+				if (indexListsEqual(solution, list)) return;
+			}
+
+			solutionList.add(list);
 		}
 	}
 
@@ -113,7 +247,42 @@ public class NQueens2 {
 		System.out.println();
 	}
 
+	private static void usage() {
+		System.err.println("NQueens2 [-display-results] [-n <#>] [-all-solutions] [-allow-colinear-boards]");
+		System.exit(-1);
+	}
+
 	public static void main(String[] arg) {
-		new NQueens2(Integer.parseInt(arg[0]));
+		int n = 8;
+
+		for (int i=0; i<arg.length; i++) {
+			if (arg[i].equals(N_ARG) && arg.length > i + 1) {
+				n = Integer.parseInt(arg[++i]);
+				continue;
+			}
+
+			if (arg[i].equals(RESULT_ARG)) {
+				displayResults = true;
+				continue;
+			}
+
+			if (arg[i].equals(COLINEAR_ARG)) {
+				colinearBoards = true;
+				continue;
+			}
+
+			if (arg[i].equals(SOLUTION_ARG)) {
+				fundamentalSolutions = false;
+				continue;
+			}
+
+			usage();
+		}
+
+		//Debug.log("- fundamentalSolutions: " + fundamentalSolutions);
+		//Debug.log("- colinearBoards: " + colinearBoards);
+		//Debug.log("- displayResults: " + displayResults);
+
+		new NQueens2(n);
 	}
 }
